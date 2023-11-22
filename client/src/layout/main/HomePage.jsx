666fd2ca3fc765e787dd3c74130c2b40bd193d8a
omer2604 <omermymon@gmail.com>
2023-11-22 17:55:49 +0200

    changes

--- client/src/layout/main/HomePage.jsx
+++ client/src/layout/main/HomePage.jsx
@@ -28,29 +28,31 @@ class HomePage extends Component {
         <div className="date-time">{currentTime.toLocaleString()}</div>;
         <img className="logoHome" src="/assets/images/logo.png" alt="לוגו" />
         <PageHeader title="Eden Cakes" subTitle="!ברוכים הבאים לאתר שלנו" />
-        <span>
-          <p className="Line1">
-            !אז לאחר הרבה זמן סוף סוף האתר הרשמי שלנו עלה לאוויר
+        <div
+          style={{
+            direction: "rtl",
+            maxWidth: "500px",
+            margin: "20px auto",
+            padding: "50px",
+            boxShadow: "0 0 10px rgba(0,0,0,0.1)",
+            borderRadius: "8px",
+            backgroundColor: "pink",
+          }}
+        >
+          <span>
+            <p className="Line1">
+              אז לאחר הרבה זמן סוף סוף האתר הרשמי שלנו עלה לאוויר
+            </p>
+          </span>
+          <p className="Line2">
+            באתר זה תוכלו לראות חלק מהמוצרים שלנו, לבצע הזמנות, לראות מתכונים
+            ועוד
           </p>
-        </span>
-        <p className="Line2">
-          באתר זה תוכלו לראות חלק מהמוצרים שלנו, לבצע הזמנות, לראות מתכונים ועוד
-        </p>
-        <p className="Line3">
-          על מנת לשוטט באתר יש להיעזר בסרגל הניווט שנמצא בחלק העליון של הדף
-        </p>
-        <p className="Line4">!גלישה מהנה</p>
-        <img
-          className="pic"
-          src="/assets/images/alphaphores.png"
-          alt="alphaphores"
-        />
-        <img
-          className="pic"
-          src="/assets/images/heartCake.png"
-          alt="heart cake"
-        />
-        <img className="pic" src="/assets/images/puffs.png" alt="puffs" />
+          <p className="Line3">
+            על מנת לשוטט באתר יש להיעזר בסרגל הניווט שנמצא בחלק העליון של הדף
+          </p>
+          <p className="Line4">גלישה מהנה</p>
+        </div>
         <p className="survey">
           {" "}
           <a href="/survey"> רוצים להגיד לנו מה אתם חושבים עלינו? לחצו כאן </a>
